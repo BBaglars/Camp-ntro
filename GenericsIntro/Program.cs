77f596872757221767f64b4f077c953b9123e7ee
Berkay Bağlars <baglarsberkay38@gmail.com>
2023-07-10 15:55:27 +0300

    10 Temmuz Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,9 @@
+﻿using GenericsIntro;
+
+//Count = eleman sayısı
+
+MyList<string> isimler = new MyList<string>();
+isimler.Add("Engin");
+
+List<string> liste = new List<string>();
+Console.WriteLine(liste.Count);
---
+++ OOP1/Program.cs
@@ -0,0 +1,21 @@
+﻿using OOP1;
+
+Product product1 = new Product();
+product1.Id = 1;
+product1.CategoryId = 2;
+product1.ProductName = "Masa";
+product1.UnitPrice = 500;
+product1.UnıtInStock = 3;
+
+Product product2 = new Product {Id = 2, CategoryId = 5, UnıtInStock = 5, 
+    ProductName = "Kalem", UnitPrice = 35};
+
+//PascalCase //camelCase
+//case sensitive = Küçük büyük harf duyarlı 
+ProductManager productManager = new ProductManager();
+productManager.Add(product1);
+
+productManager.Topla2(3, 6);
+
+int toplamsonuc = productManager.Topla(3, 6);
+Console.WriteLine(toplamsonuc*2);
