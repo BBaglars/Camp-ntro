a072448da7c661fcb1df53308ae711cf4f0728e2
Berkay Bağlars <baglarsberkay38@gmail.com>
2023-07-14 17:59:08 +0300

    14 Temmuz Ders Sonu

---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    //İnterface leri biribirinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanırız
+    interface IKrediManager
+    {
+        void Hesapla();
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,19 @@
+﻿using OOP3;
+using System.Collections.Generic;
+
+IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+IKrediManager tasitKrediManager = new TasitKrediManager();
+IKrediManager konutKrediManager = new KonutKrediManager();
+
+ILoggerService databaseloggerService = new DatabaseLoggerService();
+ILoggerService fileLoggerService = new FileLoggerService();
+
+List<ILoggerService> loggers = new List<ILoggerService> {new DatabaseLoggerService(), new SmsLoggerService()};
+
+BasvuruManager basvuruManager = new BasvuruManager();
+basvuruManager.BasvuruYap(new EsnafKredisiManager(),new List<ILoggerService> {new DatabaseLoggerService(), new SmsLoggerService()});
+basvuruManager.BasvuruYap(new EsnafKredisiManager(), loggers);
+
+List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+
+//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
